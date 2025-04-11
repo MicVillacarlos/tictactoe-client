7062858b36fbe64984bc924be6f2d943f52c54bf
MicVillacarlos <micvillacarlos@gmail.com>
2025-04-11 22:04:55 +0800

    add pages

---
+++ src/app/game/game.tsx
@@ -0,0 +1,9 @@
+"use client"
+import React from "react";
+import Layout from "../components/Organisms/layout/Layout";
+
+const Game = () => {
+  return <Layout>Game</Layout>;
+};
+
+export default Game;
---
+++ src/app/history/history.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+import Layout from "../components/Organisms/layout/Layout";
+
+const History = () => {
+  return <Layout>History</Layout>;
+};
+
+export default History;
